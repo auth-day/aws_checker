e01268748c01a04e24405fb36985c2b9f8f49377
Dmitry Bogatyr <diboan@yandex.ru>
2018-09-12 16:25:36 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,16 @@ from setuptools import setup, find_packages
 from os.path import join, dirname
 from distutils.core import setup
 
+class register(register_orig):
+
+    def _get_rc_file(self):
+        return os.path.join('.', '/opt/.pypirc')
+
+class upload(upload_orig):
+
+    def _get_rc_file(self):
+        return os.path.join('.', '/opt/.pypirc')
+
 setup(
     name = 'aws_checker',
     packages = find_packages(),
@@ -12,4 +22,8 @@ setup(
     include_package_data=True,
     url = 'https://github.com/diboanches/aws_checker',
     download_url = 'https://github.com/diboanches/aws_checker',
+    cmdclass={
+        'register': register,
+        'upload': upload,
+    }
 )
