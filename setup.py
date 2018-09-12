3c0926cb714579baff2478b0f5cd1f3bc99c6600
Dmitry Bogatyr <diboan@yandex.ru>
2018-09-12 16:59:29 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class upload(upload_orig):
 setup(
     name = 'aws_checker',
     packages = find_packages(),
-    version = '0.2',
+    version = '1.0',
     description = 'List AWS resources',
     author = 'diboan',
     author_email = 'diboan@yandex.ru',
